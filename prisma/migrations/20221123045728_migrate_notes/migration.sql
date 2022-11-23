d5fd5165de88853f5c55f869334a682d0e44ab6c
kiencx <kiencx@appota.com>
2022-11-23 14:13:35 +0700

    first commit

---
+++ prisma/migrations/20221123045728_migrate_notes/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Projects` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Tasks` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Tasks" DROP CONSTRAINT "Tasks_projectId_fkey";
+
+-- DropTable
+DROP TABLE "Projects";
+
+-- DropTable
+DROP TABLE "Tasks";
+
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
+);
